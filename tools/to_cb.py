3bc69b7c58dee2a3cf2318d5f2e3a97ffc1dcfd5
sxclij <sxclij@gmail.com>
2024-10-18 20:10:20 +0900

    add setblock

--- tools/to_cb.py
+++ tools/to_cb.py
@@ -11,10 +11,20 @@ with open(path, 'r') as file:
 time.sleep(1)
 
 for (i, line) in enumerate(src.splitlines()):
+
     line_escaped = line.replace("\"", "\\\"")
     pos = "~ ~" + str(i) + " ~1 "
-    out = "/data modify block " + pos + "Command set value \"" + line_escaped + "\""
-    pyperclip.copy(out)
+    out2 = "/data modify block " + pos + "Command set value \"" + line_escaped + "\""
+    
+    if i == 0:
+        out1 = "/setblock " + pos + "minecraft:repeating_command_block[facing=up]"
+    else:
+        out1 = "/setblock " + pos + "minecraft:chain_command_block[facing=up]"
+    pyperclip.copy(out1)
+    pyautogui.hotkey('t')
+    pyautogui.hotkey('ctrl', 'v')
+    pyautogui.hotkey('enter')
+    pyperclip.copy(out2)
     pyautogui.hotkey('t')
     pyautogui.hotkey('ctrl', 'v')
     pyautogui.hotkey('enter')
